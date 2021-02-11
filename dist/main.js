360485e66c4c37da01697aefcb0096aa3fcbaf7a
safeplace12345 <safeplace12345@gmail.com>
2021-02-11 19:46:17 +0200

    Drop down done

---
+++ dist/main.js
@@ -0,0 +1,33 @@
+(function () {
+    'use strict';
+
+    document.querySelector('.root');
+    const btn = document.querySelector('.btn');
+
+    function displayOpts() {
+        const opts = document.querySelectorAll('.option');
+        opts.forEach((opt) => {
+            const el = opt;
+            el.style.display = 'block';
+        });
+    }
+
+    function hideOpts() {
+        const opt = document.querySelectorAll('.option');
+        // eslint-disable-next-line no-return-assign
+        opt.forEach((op) => {
+            const elem = op;
+            elem.style.display = 'none';
+        });
+    }
+    btn.addEventListener('mouseover', displayOpts);
+    btn.addEventListener('mouseleave', hideOpts);
+
+    exports.dropDown = function () {
+        return {
+            displayOpts,
+            hideOpts
+        }
+    };
+
+}());
